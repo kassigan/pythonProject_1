0501facce2aeb76641c1135d402077785ed80403
kassigan <ksenialebedeva13@gmail.com>
2025-10-15 23:28:19 +0200

    scrapy divan

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,19 @@
+
+# scrap all lamps from the website: name, price, link
+
+import scrapy
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lamps = response.css('div.X68VV')
+        for lamp in lamps:
+            yield {
+                'name' : lamp.css('div.LsooF  span::text').get(),
+                'price' : lamp.css('div.pY3d2  span::text').get(),
+                'url' : lamp.css('a').attrib['href']
+
+            }
